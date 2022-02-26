03c63e83792651efe06fad0e6f263fb65097a506
emustafaa <edward.mustafaa@yahoo.com>
2022-02-25 23:41:58 -0600

    added my updates

--- Assets/Scripts/ScoreScreen.cs
+++ Assets/Scripts/ScoreScreen.cs
@@ -6,11 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class ScoreScreen : MonoBehaviour
 {
+    
+
     private Transform headlineContainer;
     private Transform headlineTemplate;
 
+    [HideInInspector]
     public Texture2D image;
-    public string headlines;
+    private string headlines;
+    private int numHeadlines = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +23,13 @@ public class ScoreScreen : MonoBehaviour
         //Cursor.visible = true;
         //Cursor.lockState = CursorLockMode.None;
     }
-/*
+
     void Awake()
     {
+
         
-        headlineContainer = transform.Find("HeadlineContainer");
-        headlineTemplate = headlineContainer.Find("HeadlineTemplate");
-        headlines = PlayerPrefs.GetString("headline");
-        Debug.Log("headline on game over is " + headlines);
-        //headlineTemplate.Find("HeadlineText").GetComponent<Text>().text = headline;
-        headlineTemplate.Find("HeadlineText").GetComponent<Text>().text = PlayerPrefs.GetString("headline");
-        //headlineTemplate.Find("ClicksText").GetComponent<Text>().text = "clicks: " + score.ToString();
-        headlineTemplate.Find("ClicksText").GetComponent<Text>().text = "clicks: " + PlayerPrefs.GetInt("score").ToString();
-        headlineTemplate.Find("RawImage").GetComponent<RawImage>().texture = image;
     }
-*/
+
 
     public void Setup(int score, string headline)
     {
@@ -46,10 +42,13 @@ public class ScoreScreen : MonoBehaviour
         this.gameObject.SetActive(true);
 
         headlines = PlayerPrefs.GetString("headline");
-        Debug.Log("headline on game over is " + headlines);
+        numHeadlines = PlayerPrefs.GetInt("headlinesRemaining");
+
+        
         headlineTemplate.Find("HeadlineText").GetComponent<Text>().text = headline;
         //headlineTemplate.Find("HeadlineText").GetComponent<Text>().text = PlayerPrefs.GetString("headline");
-        headlineTemplate.Find("ClicksText").GetComponent<Text>().text = "clicks: " + score.ToString();
+        headlineTemplate.Find("ScoreText").GetComponent<Text>().text = "Your Score: " + score.ToString();
+        headlineTemplate.Find("NumberHeadlinesText").GetComponent<Text>().text = "Headlines Left: " + numHeadlines.ToString();
         //headlineTemplate.Find("ClicksText").GetComponent<Text>().text = "clicks: " + PlayerPrefs.GetInt("score").ToString();
         headlineTemplate.Find("RawImage").GetComponent<RawImage>().texture = image;
 
@@ -59,6 +58,8 @@ public class ScoreScreen : MonoBehaviour
     {
         Debug.Log("scene: " + PlayerPrefs.GetString("_last_scene_"));
         //SceneManager.LoadScene(PlayerPrefs.GetString("_last_scene_"));
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
         this.gameObject.SetActive(false);
     }
 
